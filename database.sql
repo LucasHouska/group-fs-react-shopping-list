9f21c6d73bfdbd7d88c70137c296078b78866dc9
Maggie Jenkins <adair.jenkins@gmail.com>
2022-03-24 13:34:31 -0500

    add database.sql

--- database.sql
+++ database.sql
@@ -1,2 +1,10 @@
--- Don't forget to add your create table SQL 
--- It is also helpful to include some test data
+CREATE TABLE "items" (
+	"id" SERIAL PRIMARY KEY,
+	"name" varchar(80) NOT NULL,
+	"quantity" DECIMAL NOT NULL,
+	"unit" varchar(20) NOT NULL,
+	"purchased" BOOLEAN DEFAULT false
+);
+
+INSERT INTO "items" ("name", "quantity", "unit")
+VALUES ('gum', 4, 'packs'), ('bananas', 2, 'bunches'), ('coffee', .5, 'lbs')
